329cf8ed1d13142b37d05640b3c0923f82de3573
Bperez4197 <bryceperez31@gmail.com>
2017-12-26 04:16:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,9 +4,11 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
-  
-  def initialize(name,artist,genre)
 
+  def initialize(name,artist,genre)
+    @name = name
+    @@artists << @artist = artist
+    @@genres << @genre = genre
       @@count += 1
   end
   end

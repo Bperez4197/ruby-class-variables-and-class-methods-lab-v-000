3f940f426fc3b2d9bb706b0df92750d49d629a72
Bperez4197 <bryceperez31@gmail.com>
2017-12-26 04:15:02 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,11 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-  @@artists 
-  @@genres
+  @@artists = []
+  @@genres = []
+  
   def initialize(name,artist,genre)
-    
+
       @@count += 1
   end
   end
